bd4b21f2c01af7dc4d7c654d6aaaadbb0143ba18
MSharifM <MSharifMdev@gmail.com>
2025-05-22 17:24:03 +0330

    Display the table in a graphical way

--- AI/Form1.Designer.cs
+++ AI/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnInputFile = new System.Windows.Forms.Button();
+            this.webBQuestion = new System.Windows.Forms.WebBrowser();
+            this.webBSolve = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -38,21 +42,63 @@
             // 
             // btnInputFile
             // 
-            this.btnInputFile.Font = new System.Drawing.Font("Nazanin", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
-            this.btnInputFile.Location = new System.Drawing.Point(192, 18);
+            this.btnInputFile.BackColor = System.Drawing.Color.Lime;
+            this.btnInputFile.Font = new System.Drawing.Font("Nazanin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnInputFile.ForeColor = System.Drawing.Color.Black;
+            this.btnInputFile.Location = new System.Drawing.Point(216, 18);
             this.btnInputFile.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnInputFile.Name = "btnInputFile";
             this.btnInputFile.Size = new System.Drawing.Size(100, 35);
             this.btnInputFile.TabIndex = 0;
             this.btnInputFile.Text = "انتخاب فایل";
-            this.btnInputFile.UseVisualStyleBackColor = true;
+            this.btnInputFile.UseVisualStyleBackColor = false;
             this.btnInputFile.Click += new System.EventHandler(this.btnInputFile_Click);
+            // 
+            // webBQuestion
+            // 
+            this.webBQuestion.Location = new System.Drawing.Point(12, 61);
+            this.webBQuestion.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBQuestion.Name = "webBQuestion";
+            this.webBQuestion.Size = new System.Drawing.Size(250, 232);
+            this.webBQuestion.TabIndex = 1;
+            // 
+            // webBSolve
+            // 
+            this.webBSolve.Location = new System.Drawing.Point(270, 61);
+            this.webBSolve.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBSolve.Name = "webBSolve";
+            this.webBSolve.Size = new System.Drawing.Size(250, 232);
+            this.webBSolve.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nazanin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(113, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "سوال";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nazanin", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(370, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "پاسخ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(532, 471);
+            this.ClientSize = new System.Drawing.Size(532, 378);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBSolve);
+            this.Controls.Add(this.webBQuestion);
             this.Controls.Add(this.btnInputFile);
             this.Font = new System.Drawing.Font("Nazanin", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -60,6 +106,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +114,10 @@
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnInputFile;
+        private System.Windows.Forms.WebBrowser webBQuestion;
+        private System.Windows.Forms.WebBrowser webBSolve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- AI/Form1.cs
+++ AI/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,7 +24,7 @@ namespace AI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         public void ReadInputFile(string pathFile)
@@ -56,6 +57,54 @@ namespace AI
             }
         }
 
+        public void OutputFile(string path , Box box , bool solve = false)
+        {
+            path += ".html";
+
+            #region Output
+
+            using (StreamWriter stream = new StreamWriter(path))
+            {
+                stream.WriteLine("<!DOCTYPE html>\r\n<html lang=\"fa\">\r\n<head>\r\n  <meta charset=\"UTF-8\">\r\n  <title>جدول کاکورو دقیق</title>\r\n  <style>\r\n    table.kakuro {\r\n      border-collapse: collapse;\r\n      direction: ltr;\r\n    }\r\n    table.kakuro td {\r\n      width: 40px;\r\n      height: 40px;\r\n      border: 1px solid #222;\r\n      text-align: center;\r\n      vertical-align: middle;\r\n      position: relative;\r\n      font-family: Tahoma, Arial, sans-serif;\r\n      font-size: 16px;\r\n      padding: 0;\r\n      box-sizing: border-box;\r\n    }\r\n    .black {\r\n      background: #222;\r\n    }\r\n    .white {\r\n      background: #fff;\r\n    }\r\n    .clue {\r\n      background: #222;\r\n      color: #fff;\r\n      overflow: hidden;\r\n      position: relative;\r\n    }\r\n    .clue svg {\r\n      position: absolute;\r\n      top: 0; left: 0;\r\n      width: 100%; height: 100%;\r\n      pointer-events: none;\r\n    }\r\n    /* عدد بالا راست */\r\n    .clue .clue-top {\r\n      position: absolute;\r\n      top: 2px; right: 4px;\r\n      font-size: 14px;\r\n      z-index: 2;\r\n    }\r\n    /* عدد پایین چپ */\r\n    .clue .clue-bottom {\r\n      position: absolute;\r\n      bottom: 2px; left: 4px;\r\n      font-size: 14px;\r\n      z-index: 2;\r\n    }\r\n  </style>\r\n</head>\r\n<body>\r\n  <table class=\"kakuro\">");
+                for (int i = 0; i < box.Row; i++)
+                {
+                    stream.WriteLine("<tr>");
+                    for (int j = 0; j < box.Column; j++)
+                    {
+                        if (box.data[i, j] is int number)
+                        {
+                            if (number == -1)
+                                stream.WriteLine("<td class=\"black\"></td>");
+                            else
+                                stream.WriteLine($"<td class=\"white\">{number}</td>");
+                        }
+                        else if (box.data[i, j] is Node)
+                        {
+                            Node node = box.data[i, j] as Node;
+                            if (node.Up == -1)
+                                stream.WriteLine($"<td class=\"clue black\"><div class=\"clue-bottom\">{node.Down}</div></td>");
+                            else if (node.Down == -1)
+                                stream.WriteLine($"<td class=\"clue black\"><div class=\"clue-top\">{node.Up}</div></td>");
+                            else
+                                stream.WriteLine($"<td class=\"clue black\"><div class=\"clue-top\">{node.Up}</div><div class=\"clue-bottom\">{node.Down}</div></td>");
+                        }
+                        else
+                        {
+                            stream.WriteLine("<td class=\"white\"></td>");
+                        }
+                    }
+                    stream.WriteLine("</tr>");
+                }
+            }
+
+            if(!solve) 
+                webBQuestion.Url = new Uri("C:\\Users\\Lenovo\\source\\repos\\AI\\AI\\bin\\Debug\\" + path);
+            else
+                webBSolve.Url = new Uri("C:\\Users\\Lenovo\\source\\repos\\AI\\AI\\bin\\Debug\\" + path);
+            
+            #endregion
+        }
+
         private void btnInputFile_Click(object sender, EventArgs e)
         {
             string pathFile = "";
@@ -63,8 +112,12 @@ namespace AI
             {
                 pathFile = openFileDialog1.FileName;
                 ReadInputFile(pathFile);
+                OutputFile("question" , Box);
             }
+
+            
         }
+
     }
 
     public class Box
@@ -83,7 +136,7 @@ namespace AI
 
         #region IsValid
 
-        public bool IsValidRowAndColumn(int row , int col)
+        public bool IsValidRowAndColumn(int row, int col)
         {
             if (IsValidRow(this, row, col) && IsValidColumn(this, row, col))
                 return true;
@@ -91,12 +144,12 @@ namespace AI
             return false;
         }
 
-        private bool IsValidRow (Box box , int row , int col)
+        private bool IsValidRow(Box box, int row, int col)
         {
             int target = (box.data[row, col] as Node).Up;
 
             int sum = 0;
-            for (int i = col + 1; i < box.Column && int.Parse(box.data[row, i].ToString()) != -1 ; i++)
+            for (int i = col + 1; i < box.Column && int.Parse(box.data[row, i].ToString()) != -1; i++)
             {
                 sum += int.Parse(box.data[row, i].ToString());
             }
@@ -106,12 +159,12 @@ namespace AI
             return false;
         }
 
-        private bool IsValidColumn (Box box , int row , int col)
+        private bool IsValidColumn(Box box, int row, int col)
         {
             int target = (box.data[row, col] as Node).Down;
 
             int sum = 0;
-            for (int i = row + 1; i < box.Row && int.Parse(box.data[i, col].ToString()) != -1 ; i++)
+            for (int i = row + 1; i < box.Row && int.Parse(box.data[i, col].ToString()) != -1; i++)
             {
                 sum += int.Parse(box.data[i, col].ToString());
             }
